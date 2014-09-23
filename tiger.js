904a35668a01deea477637b90c7744118e1752d1
meewog <mehrdad.dastgir@gmail.com>
2014-09-23 12:10:57 +0800

    added the jquery file

--- tiger.js
+++ tiger.js
@@ -5,3 +5,9 @@ Company: 	Tricor Senedi Sdn Bhd
 Date: 		22 September 2014
 ----------------------------------
 */
+$(
+	function()
+	{
+		console.log('Tiger tiger burning bright, in the shadow of the night')
+	}
+)
