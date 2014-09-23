a4f3bb08ecedc46203432bfe51f6869193e42e42
meewog <mehrdad.dastgir@gmail.com>
2014-09-23 15:04:44 +0800

    added a main container and custom css

--- tiger.js
+++ tiger.js
@@ -9,5 +9,30 @@ $(
 	function()
 	{
 		console.log('Tiger tiger burning bright, in the shadow of the night')
+		drawElements()
 	}
 )
+
+function drawElements()
+{
+	var html=[]
+
+	html.push
+	(
+		'<img src="img/branch.png" class="flora" id="branch" />',
+		'<img src="img/leaf01.png" class="flora" id="leaf1" />',
+		'<img src="img/leaf02.png" class="flora" id="leaf2" />',
+		'<img src="img/leaf03.png" class="flora" id="leaf3" />',
+		'<img src="img/leaf04.png" class="flora" id="leaf4" />',
+		'<img src="img/leaf05.png" class="flora" id="leaf5" />',
+		'<img src="img/leaves_group01.png" class="flora" id="bush1" />',
+		'<img src="img/leaves_group02.png" class="flora" id="bush2" />',
+		'<img src="img/leaves_group03.png" class="flora" id="bush3" />',
+		'<img src="img/tiger.png" class="fauna" id="tigerHead" />'
+	)
+
+	$('#loadHere').append
+	(
+		html.join(' ')
+	)
+}
